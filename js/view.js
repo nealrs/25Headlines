7b9cda146490f01ef60f6f4634bc32d015a8ec72
Neal Shyam <neal@audioshocker.com>
2014-12-20 15:43:07 -0500

    remove sentence/title casing from viewer

--- js/view.js
+++ js/view.js
@@ -3,48 +3,6 @@
 * Copyright © 2008–2013 David Gouch. Licensed under the MIT License.
 */
 
-String.prototype.toTitleCase = function(){
-  var smallWords = /^(a|an|and|as|at|but|by|en|for|if|in|nor|of|on|or|per|the|to|vs?\.?|via)$/i;
-
-  return this.replace(/[A-Za-z0-9\u00C0-\u00FF]+[^\s-]*/g, function(match, index, title){
-    if (index > 0 && index + match.length !== title.length &&
-      match.search(smallWords) > -1 && title.charAt(index - 2) !== ":" &&
-      (title.charAt(index + match.length) !== '-' || title.charAt(index - 1) === '-') &&
-      title.charAt(index - 1).search(/[^\s-]/) < 0) {
-        return match.toLowerCase();
-      }
-      if (match.substr(1).search(/[A-Z]|\../) > -1) {
-        return match;
-      }
-      return match.charAt(0).toUpperCase() + match.substr(1);
-    });
-  };
-
-/*
-Stack Overflow: http://stackoverflow.com/questions/19089442/convert-string-to-sentence-case-in-javascript
-*/
-String.prototype.toSentenceCase = function(){
-  //var string="Hi All, This Is Derp. Thank You All to Answer My Query";
-  var n=this.toLowerCase().split(".");
-  var vfinal="";
-  for(i=0;i<n.length;i++)
-    {
-      var spaceput="";
-      var spaceCount=n[i].replace(/^(\s*).*$/,"$1").length;
-      n[i]=n[i].replace(/^\s+/,"");
-      var newstring=n[i].charAt(n[i]).toUpperCase() + n[i].slice(1);
-      for(j=0;j<spaceCount;j++)
-        spaceput=spaceput+" ";
-        vfinal=vfinal+spaceput+newstring+".";
-      }
-      vfinal=vfinal.substring(0, vfinal.length - 1);
-      vfinal=vfinal.replace(/(\si'\s)/, " I' ");
-      vfinal=vfinal.replace(/(\si\s)/, " I ");
-
-      //console.log(vfinal);
-      return vfinal;
-};
-
 /*CSS Tricks: http://css-tricks.com/snippets/javascript/get-url-variables/*/
 function getId(variable){
   var query = window.location.search.substring(1);
@@ -105,13 +63,6 @@ $( document ).ready(function() {
       $('#len1').text(hy);
       $('#len2').text(ho);
       $('#len3').text(hr);
-
-      // reapply title casing
-      $('.hl').each(function(k,v){
-        if(v.value){
-          v.value = v.value.toTitleCase();
-        }
-      });
     }
 
     if ( type == "Tweets" ){
@@ -119,13 +70,6 @@ $( document ).ready(function() {
       $('#len1').text(ty);
       $('#len2').text(to);
       $('#len3').text(tr);
-
-      // reapply sentence casing
-      $('.hl').each(function(k,v){
-        if(v.value){
-          v.value = v.value.toSentenceCase();
-        }
-      });
     }
 
     if ( type == "Subject lines" ){
@@ -134,12 +78,6 @@ $( document ).ready(function() {
       $('#len2').text(eo);
       $('#len3').text(er);
 
-      // reapply sentence casing
-      $('.hl').each(function(k,v){
-        if(v.value){
-          v.value = v.value.toSentenceCase();
-        }
-      });
     }
     // recheck all inputs against length triggers
     colorCheckAll();
